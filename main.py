aad56f697ef658fb3d54bd3bbec6a9d70ec677a0
Bernie L <lawsonbh@gmail.com>
2022-08-03 22:32:05 -0400

    Added function for working with s3 uploads

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
-
+import boto3
+from botocore.exceptions import ClientError
+import logging
+import os
 
 app = FastAPI()
 
@@ -15,3 +18,23 @@ def fetch_bee_photo(file_path: str):
 @app.delete("/bees/{file_path:path}")
 def delete_bee_photo(file_path: str):
     return {"file_path": file_path}
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to S3 bucket
+    :param file_name: File to upload
+    :param bucket: Bucket in s3 to upload to
+    :param object_name: S3 object name, default to file_name parameter value
+    :return: True if file uploaded, else False
+    """
+
+    if object_name is None:
+        object_name = os.path.basename(file_name
+    
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
